b5f22ec113775b3e8b1dc4e668edb580043e5123
cristianbyte <charlsmaritz@gmail.com>
2024-04-10 18:01:01 -0500

    feat(auth): refactor authorization token handling in user and pairing services

--- src/services/userService/createUser.js
+++ src/services/userService/createUser.js
@@ -2,25 +2,30 @@ const API_URL = 'http://192.168.1.7:8080/api/v1';
 
 export const createUser = async (userData) => {
   try {
-      const response = await fetch(`${API_URL}/user`, {
-          method: 'POST',
-          headers: {
-            'Authorization': `Bearer ${userData.myuuid}`,
-            'Content-Type': 'application/json',
-            'Accept': '*/*'
-          },
-          body: JSON.stringify(userData)
-      });
-      
-      const data = await response.json();
-      
-      if (!response.ok) {
-          console.log(data);
-      }
-      
-      return data;
+    const response = await fetch(`${API_URL}/user/auth`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': '*/*'
+      },
+      body: JSON.stringify(userData)
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error('Error response:', errorText);
+      throw new Error(`Request failed with status ${response.status}`);
+    }
+
+    const responseData = await response.json();
+
+    localStorage.setItem('token', responseData.token);
+    console.log(responseData.token);
+
+    return responseData.user;
+    
   } catch (error) {
-      console.error('Error creating user:', error);
-      throw error;
+    console.error('Error creating user:', error);
+    throw error;
   }
 };
